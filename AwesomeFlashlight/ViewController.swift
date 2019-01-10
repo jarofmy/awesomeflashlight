65ac69f5b89ebc51d28fca8c8b7467f147313667
Jaro <jeremyvan614@gmail.com>
2019-01-09 19:51:10 -0600

    flashlight example app from class day 1

--- AwesomeFlashlight/ViewController.swift
+++ AwesomeFlashlight/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func toggledSwitch(_ sender: Any) {
+        if (view.backgroundColor == UIColor.black) {
+            view.backgroundColor = UIColor.yellow
+            titleLabel.textColor = UIColor.black
+            subtitleLabel.textColor = UIColor.yellow
+        } else {
+            view.backgroundColor = UIColor.black
+            titleLabel.textColor = UIColor.white
+            subtitleLabel.textColor = UIColor.black
+        }
+    }
 }
 
