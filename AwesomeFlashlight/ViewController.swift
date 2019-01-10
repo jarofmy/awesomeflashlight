4e79ca29b0107904e74d46a37ec2b9c41bebb3c3
Jaro <jeremyvan614@gmail.com>
2019-01-09 19:14:23 -0600

    Initial Commit

---
+++ AwesomeFlashlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeFlashlight
+//
+//  Created by Jeremy Van on 1/9/19.
+//  Copyright © 2019 Jeremy Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
